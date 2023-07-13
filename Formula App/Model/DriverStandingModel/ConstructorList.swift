7163c26707ff8445639c038fe6ca059d96a3ab9f
riccardo <riccardo.pischtschan@gmail.com>
2023-07-13 17:26:59 +0200

    test

---
+++ Formula App/Model/DriverStandingModel/ConstructorList.swift
@@ -0,0 +1,16 @@
+//
+//  ConstructorList.swift
+//  Formula App
+//
+//  Created by Riccardo Pischtschan on 13.07.23.
+//
+
+import Foundation
+
+struct ConstructorList: Codable,Hashable {
+    let constructorId: String?
+//    let url: String
+    let name: String?
+    let nationality: String?
+}
+
