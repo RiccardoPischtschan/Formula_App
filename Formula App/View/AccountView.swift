40129babdf834dbba46fa1b37d1a3cf7c829524e
riccardo <riccardo.pischtschan@gmail.com>
2023-07-21 09:57:09 +0200

    AccountView gefixt picker wheel funktioniert jetzt wieder

--- Formula App/View/AccountView.swift
+++ Formula App/View/AccountView.swift
@@ -10,11 +10,13 @@ import FirebaseAuth
 
 struct AccountView: View {
     
-    @EnvironmentObject var firebaseAuth: FirebaseAuthService
+    
     @EnvironmentObject var dataManager : DataManager
     @State private var selectedMode = 0
     private let mode = ["F1","Light","Dark","Red Bull","Mercedes","Aston Martin","Ferrari","McLaren","Alpine","Williams","Haas","Alfa Romeo","Alpha Tauri"]
-    @State private var name = ""
+    @State var name = ""
+    
+    
     var body: some View {
         NavigationView{
             ZStack{
@@ -27,27 +29,48 @@ struct AccountView: View {
                     .offset(y: -250)
                 
                 VStack{
-                    AccountInfoView()
+                    
+                    HStack{
+                        Text("App Style")
+                            .foregroundColor(.red)
+                        
+                        Picker("", selection: $selectedMode) {
+                            ForEach(0 ..< mode.count) { index in
+                                Text(mode[index])
+                                    .foregroundColor(Color(hue: 1.0, saturation: 0.878, brightness: 0.988, opacity: 0.949))
+                            }
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(width: 140, height: 100)
+                        .padding()
+                    }
+                    .padding()
+                    
+                    HStack{
+                        Image(systemName: "person.crop.rectangle")
+                            .foregroundColor(.white)
+                        
+                        
+                            TextField("", text: $name, prompt: Text(dataManager.currentUser.name).foregroundColor(.white))
+                                .foregroundColor(.white)
+                       
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 2)
+                            .foregroundColor(.white)
+                    )
+                    .frame(width: 300)
+                    .padding()
+//                    .offset(y: 30)
+                    
+                    
+                    AccountInfoView(selectedMode: $selectedMode, name: $name)
                         .frame(height: 300)
                         .environmentObject(dataManager)
-                                  
-                    Button{
-                        
-                        firebaseAuth.signOut()
-                        
-                        
-                    } label: {
-                        Text("Log Out")
-                            .bold()
-                            .frame(width: 200,height: 40)
-                            .background(
-                                RoundedRectangle(cornerRadius: 10, style: .continuous)
-                                    .fill(.linearGradient(colors: [.red,.white], startPoint: .top, endPoint: .bottomTrailing ))
-                            )
-                            .foregroundColor(.black)
-                    }
-                    .padding(.top)
-                    .offset(y: 100)
+                   
                 }
             }
         }
@@ -59,7 +82,7 @@ struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
         AccountView()
             
-            .environmentObject(FirebaseAuthService())
+          
             .environmentObject(DataManager())
     }
 }
